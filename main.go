cb60b8f5e92fbeee4ff26b4564cc5d7ec25f9d07
Steven Jansen <stevenjansen180@gmail.com>
2022-12-13 19:49:51 +0100

    Revert "Revert "ok""
    This reverts commit c7fb585126858613c737f42c18c8fa4873ca3286.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"Gateway/pkg/config"
 	"Gateway/pkg/http/rest"
-	"Gateway/pkg/kafka"
 	"errors"
 	"fmt"
 	"os"
@@ -27,29 +26,6 @@ func run() error {
 	server := rest.NewServer(cfg, cfg.Environment)
 	server.Init()
 
-	// go StartKafka()
-	kafka.Init(cfg.Kafka)
-
 	server.Run(cfg.Name)
 	return nil
-}
-
-func StartKafka() {
-	// conf := kafka.ReaderConfig{
-	// 	Brokers: []string{"145.93.76.209:9092"},
-	// 	Topic:   "fromBrand",
-	// 	GroupID: "g1",
-	// }
-
-	// reader := kafka.NewReader(conf)
-
-	// fmt.Println("Started the reader")
-	// for {
-	// 	m, err := reader.ReadMessage(context.Background())
-	// 	if err != nil {
-	// 		fmt.Println("Error", err)
-	// 		continue
-	// 	}
-	// 	fmt.Println("Message:", string(m.Value))
-	// }
 }
