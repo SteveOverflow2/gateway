0ade4b47ba2c74321edb9b88f5bfeee41228b522
Steven Jansen <stevenjansen180@hotmail.com>
2022-12-14 11:09:41 +0100

    a

--- pkg/http/rest/router.go
+++ pkg/http/rest/router.go
@@ -5,6 +5,6 @@ import (
 )
 
 func (s *server) routes() {
-	s.Router.PathPrefix("import/").Handler(http.HandlerFunc(CreateReverseProxy())).Methods(http.MethodPost)
+	s.Router.PathPrefix("/import/").Handler(http.HandlerFunc(CreateReverseProxy())).Methods(http.MethodPost)
 	s.Router.PathPrefix("/{service}").Handler(http.HandlerFunc(CreateReverseProxy())).Methods(http.MethodGet, http.MethodOptions)
 }
