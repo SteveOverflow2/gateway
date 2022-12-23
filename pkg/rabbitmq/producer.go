05c3cbad7b4351bfc743e3fbf21e222f93d86b49
Steven Jansen <stevenjansen180@hotmail.com>
2022-12-23 11:06:40 +0100

    a

--- pkg/rabbitmq/producer.go
+++ pkg/rabbitmq/producer.go
@@ -22,10 +22,8 @@ func StartServer(cfg config.RabbitMQ) {
 	conn, err := amqp.Dial("amqp://guest:guest@" + cfg.Host + ":" + cfg.Port)
 	fmt.Println("Done dialing")
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
 	ch, err = conn.Channel()
 	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
 	q, err = ch.QueueDeclare(
 		"hello", // name
 		false,   // durable
