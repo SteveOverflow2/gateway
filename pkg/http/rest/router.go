d60806fdda225354c74045aff74d3ad9c5ff48c3
Steven Jansen <stevenjansen180@hotmail.com>
2022-12-23 11:00:01 +0100

    o

--- pkg/http/rest/router.go
+++ pkg/http/rest/router.go
@@ -1,11 +1,13 @@
 package rest
 
 import (
+	"Gateway/pkg/rabbitmq"
 	"net/http"
 )
 
 func (s *server) routes() {
 	// s.Router.PathPrefix("/import/").Handler(http.HandlerFunc(CreateReverseProxy())).Methods(http.MethodPost)
 	s.Router.PathPrefix("/{service}").Handler(http.HandlerFunc(CreateReverseProxy())).Methods(http.MethodGet, http.MethodOptions)
+	s.Router.PathPrefix("/{service}").Handler(http.HandlerFunc(rabbitmq.SendMessage()))
 	s.Router.HandleFunc("/", PrintRequest())
 }
--- pkg/rabbitmq/producer.go
+++ pkg/rabbitmq/producer.go
@@ -2,10 +2,18 @@ package rabbitmq
 
 import (
 	"Gateway/pkg/config"
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var (
+	ch *amqp.Channel
+	q  amqp.Queue
 )
 
 func StartServer(cfg config.RabbitMQ) {
@@ -15,10 +23,10 @@ func StartServer(cfg config.RabbitMQ) {
 	fmt.Println("Done dialing")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
-	ch, err := conn.Channel()
+	ch, err = conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
-	_, err = ch.QueueDeclare(
+	q, err = ch.QueueDeclare(
 		"hello", // name
 		false,   // durable
 		false,   // delete when unused
@@ -31,5 +39,26 @@ func StartServer(cfg config.RabbitMQ) {
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func SendMessage() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		body := "This is a test"
+		err := ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s\n", body)
+
 	}
 }
